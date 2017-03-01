fbc8ec0814bd849a643e572c6bc4315d4437a9dd
Sebastien Iooss <sebastien.iooss@student.ecp.fr>
2017-03-01 18:31:44 +0800

    First migration of database

--- main/models.py
+++ main/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=20)
+    phone = models.IntegerField()
+
+
+class Customer(User):
+    balance = models.FloatField()
+
+
+class Order(models.Model):
+    time = models.DateTimeField()
+    isServed = models.BooleanField()
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
