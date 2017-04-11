228439259192b8c15a02b9d95a5e8b24cb46b836
Harshul Gandhi <gandhi.harshul.88@gmail.com>
2017-04-12 03:04:14 +0800

    Nav bar styling, app icon and banner

---
+++ static/main/js/home.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $('#banner-slide').bjqs({
+        animtype      : 'slide',
+        height        : 360,
+        width         : 1440,
+        responsive    : true,
+        randomstart   : true,
+        automatic     : true
+    });
+});
